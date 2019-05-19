851309d0a9560aa76193b67788a7e066dbdc16dc
liang.deng <deven2012.deng@gmail.com>
2019-05-19 21:38:35 +0000

    imemory dev

--- swedish/lib/imemory/imemory.py
+++ swedish/lib/imemory/imemory.py
@@ -1,4 +1,10 @@
+import os
+import json
 
+# data dir, can be extend for configurable
+g_data_path = './data'
+g_uid2index_file = 'uid2index.json'
+g_word2index_file = 'word2index.json'
 class iMemory:
     """
 
@@ -14,10 +20,10 @@ class iMemory:
         self.simplelog("iMemory object created.")
 
         # Load data failure
-        if(not self.load_data()):
+        if(not self.load_data(uid)):
             self.simplelog("Load user data failure!")
             mActive = False
-            return None
+        mActive = True
 
     def __str__(self):
         return(self.mDesc + '\nVersion ' + self.mVersion)
@@ -26,8 +32,20 @@ class iMemory:
         if(self.mDebug):
             print(text)
 
-    def load_data(self):
-        return False
+    def load_data(self, uid):
+        index_file = os.path.join(g_data_path, g_uid2index_file)
+        index_data = {}
+        with open(index_file, 'w+') as index_fd:
+            try:
+                index_data = json.load(index_fd)
+                print(index_data)
+            except:
+                self.simplelog('Index file load failure!')
+                index_data = {}
+                index_data['index'] = 1
+            index_data['index'] += 1
+            json.dump(index_data, index_fd)
+        return True
 
     # Interfaces 
     def add_user_data(self, uid, desc):
